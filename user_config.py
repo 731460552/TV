42e8a82bbeaa1c8c8153740b98d24b92c16f53b6
731460552 <104701473+731460552@users.noreply.github.com>
2024-04-19 16:43:23 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,20 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "四川卫视",
+    "CCTV-6",
+    "CCTV-6+",
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+]
+favorite_page_num = 3
+default_page_num = 2
+urls_limit = 20
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
